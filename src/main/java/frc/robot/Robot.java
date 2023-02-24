8433b1d7eb9fe14aba78fa6ffafe4387faa1816e
Timmy McInerney <Timmybud922@gmail.com>
2023-02-23 20:59:57 -0500

    holymoly, we can see.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,9 +1,6 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
@@ -27,38 +24,37 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
 import org.photonvision.common.hardware.VisionLEDMode;
 
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto1 = "Custom Auto 1";
-  private static final String kCustomAuto2 = "Custom Auto 2";
-  private static final String kCustomAuto3 = "Custom Auto 3";
-  private static final String kCustomAuto4 = "Custom Auto 4";
-
+  private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  private final XboxController c_xbox1 = new XboxController(0);
 
-  //creates SPARK MAX motor controller devices
-  
-  private static final int LFrontDeviceID = 2; 
-  private static final int LBackDeviceID = 3;
-  private static final int RFrontDeviceID =4;
-  private static final int RBackDeviceID = 5;
-  private CANSparkMax m_LFrontMotor;
-  private CANSparkMax m_LBackMotor;
-  private CANSparkMax m_RFrontMotor;
-  private CANSparkMax m_RBackMotor;
+  public CANSparkMax m_LFrontMotor;
+    public CANSparkMax m_LBackMotor;
+    public CANSparkMax m_RFrontMotor;
+    public CANSparkMax m_RBackMotor; 
+  {
+    m_LFrontMotor = new CANSparkMax(2, MotorType.kBrushless);
+    m_LBackMotor = new CANSparkMax(3, MotorType.kBrushless);
+    m_RFrontMotor = new CANSparkMax(4, MotorType.kBrushless);
+    m_RBackMotor = new CANSparkMax(5, MotorType.kBrushless);
+  }
+  MotorControllerGroup m_left = new MotorControllerGroup(m_LFrontMotor, m_LBackMotor);
+  MotorControllerGroup m_right = new MotorControllerGroup(m_RFrontMotor, m_RBackMotor);
+  double strait;
+  double turn;
 
-  //create motor controller groups
-  MotorControllerGroup mg_leftGroup = new MotorControllerGroup(m_LFrontMotor, m_LBackMotor);
-  MotorControllerGroup mg_rightGroup = new MotorControllerGroup(m_RFrontMotor, m_RBackMotor);
 
-  DifferentialDrive m_Drive = new DifferentialDrive(mg_leftGroup, mg_rightGroup);
-  //controllers
-  XboxController c_xbox1 = new XboxController(0);
-  XboxController c_xbox2 = new XboxController(1);
-
-  //Vision Data stuff
   NetworkTableInstance inst = NetworkTableInstance.getDefault();
   NetworkTable table = inst.getTable("cam1");
    
@@ -69,200 +65,157 @@ public class Robot extends TimedRobot {
   NetworkTableEntry targetYaw;
   NetworkTableEntry pipelineIndex;
   NetworkTableEntry ledMode; 
-
-  //variables
-  public double straight;
-  public double turn;
-  public double maxspeed =(.5);
-  public double arm;
-  public double arm_var;
-  public double leftSpeed = straight+turn;
-  public double rightSpeed =straight-turn;
-  public double forwardSpeed;
-  public double rotationSpeed;
-  boolean toggleOn = false;
-  boolean togglePressed = false;
-  
-  
-  //Pneumatics
-   Solenoid example1SolenoidPCM = new Solenoid(PneumaticsModuleType.REVPH, 1);
-   Solenoid example2SolenoidPCM = new Solenoid(PneumaticsModuleType.REVPH, 2);
-
-   //PHOTONVISION INIT 
-       //TODO Constants such as camera and target height stored. Change per robot and goal!
-       final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(23.622); //done
-       final double TARGET_HEIGHT_METERS = Units.feetToMeters(5); //TODO this one
-       // Angle between horizontal and the camera.
-       final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0); //90? //done
-   
-       // How far from the target we want to be
-       final double GOAL_RANGE_METERS = Units.feetToMeters(3);
-   
-       // Change this to match the name of your camera
-       PhotonCamera camera = new PhotonCamera("cam1");
-   
-       // PID constants should be tuned per robot
-       final double LINEAR_P = 0.1;
-       final double LINEAR_D = 0.0;
-       PIDController forwardController = new PIDController(LINEAR_P, 0, LINEAR_D);
-   
-       final double ANGULAR_P = 0.1;
-       final double ANGULAR_D = 0.0;
-       PIDController turnController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
-
-
-  @Override
-  public void robotInit() { 
-    //theres probably gonna be a lot of auto things
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("Auto 1", kCustomAuto1);
-    m_chooser.addOption("Auto 2", kCustomAuto2);
-    m_chooser.addOption("Auto 3", kCustomAuto3);
-    m_chooser.addOption("Auto 4", kCustomAuto4);
+    
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_left, m_right);
+{
+  m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+}
+  // Constants such as camera and target height stored. Change per robot and goal!
+  final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(4);
+  final double TARGET_HEIGHT_METERS = Units.feetToMeters(0);
+  // Angle between horizontal and the camera.
+  final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
 
-  //SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
+  // How far from the target we want to be
+  final double GOAL_RANGE_METERS = Units.feetToMeters(3);
 
-    m_LFrontMotor = new CANSparkMax(LFrontDeviceID, MotorType.kBrushless);
-    m_LBackMotor = new CANSparkMax(LBackDeviceID, MotorType.kBrushless);
-    m_RFrontMotor = new CANSparkMax(RFrontDeviceID, MotorType.kBrushless);
-    m_RBackMotor = new CANSparkMax(RBackDeviceID, MotorType.kBrushless);
-    
-    
-    //get encoders
-    m_LFrontMotor.getEncoder();
-    m_LBackMotor.getEncoder();
-    m_RFrontMotor.getEncoder();
-    m_RBackMotor.getEncoder();
-    //SET PhotonVision PipeLine index to 0, 0 = Retroreflective 
-    camera.setPipelineIndex(0);
-    //Turn of LL LEDs.. 
-    camera.setLED(VisionLEDMode.kOff);
+  // Change this to match the name of your camera
+  PhotonCamera camera = new PhotonCamera("cam1");
 
-  }@Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    switch (m_autoSelected) {
-      case kDefaultAuto:
-      default:
-      //auto auto
-      break;
-      case kCustomAuto1:
-      // auto 
-      break;
-      case kCustomAuto2:
-      // auto 
-      break;
-      case kCustomAuto3:
-      //auto
-      break;
-      case kCustomAuto4:
-      // auto
-      break;
-      }
+  // PID constants should be tuned per robot
+  final double LINEAR_P = -0.01;
+  final double LINEAR_D = 0.0;
+  PIDController forwardController = new PIDController(LINEAR_P, 0, LINEAR_D);
+
+  final double ANGULAR_P = -0.01;
+  final double ANGULAR_D = 0.0;
+  PIDController turnController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
+
+  
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
   }
 
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {}
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+        
+    }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    turn =  c_xbox1.getRawAxis(3) - c_xbox1.getRawAxis(2);
-    straight = -c_xbox1.getLeftY();
-
-    //PhotonVision
+    strait = -c_xbox1.getLeftY();
+    turn = c_xbox1.getLeftTriggerAxis() - c_xbox1.getRightTriggerAxis();
+    m_robotDrive.arcadeDrive(strait, turn);
     double forwardSpeed;
     double rotationSpeed;
 
-    if (c_xbox1.getAButton()) { 
-      camera.setLED(VisionLEDMode.kBlink);
-        try {
-          wait(100);
-        } catch (InterruptedException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
+    
+
+    if (c_xbox1.getAButton()) {
+        // Vision-alignment mode
+        // Query the latest result from PhotonVision
+        var result = camera.getLatestResult();
+
+        if (result.hasTargets()) {
+            // First calculate range
+            double range =
+                    PhotonUtils.calculateDistanceToTargetMeters(
+                            CAMERA_HEIGHT_METERS,
+                            TARGET_HEIGHT_METERS,
+                            CAMERA_PITCH_RADIANS,
+                            Units.degreesToRadians(result.getBestTarget().getPitch()));
+
+            // Use this range as the measurement we give to the PID controller.
+            // -1.0 required to ensure positive PID controller effort _increases_ range
+            forwardSpeed = -forwardController.calculate(range, GOAL_RANGE_METERS);
+
+            // Also calculate angular power
+            // -1.0 required to ensure positive PID controller effort _increases_ yaw
+            rotationSpeed = -turnController.calculate(result.getBestTarget().getYaw(), 0);
+        } else {
+            // If we have no targets, stay still.
+            forwardSpeed = 0;
+            rotationSpeed = 0;
         }
-        camera.setLED(VisionLEDMode.kOn);
-        try {
-          wait(500);
-        } catch (InterruptedException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
-      // Vision-alignment mode
+    } else {
+        // Manual Driver Mode
+        forwardSpeed = -c_xbox1.getLeftY();
+        rotationSpeed = c_xbox1.getLeftTriggerAxis() - c_xbox1.getRightTriggerAxis();
+    }
 
-      // Query the latest result from PhotonVision
-      var RRresult = camera.getLatestResult();
-      System.out.println("Finding RR Target");
-      if (RRresult.hasTargets()) {
-        System.out.println("RR Target Found");
-          // First calculate range
-          double range =
-                  PhotonUtils.calculateDistanceToTargetMeters(
-                          CAMERA_HEIGHT_METERS,
-                          TARGET_HEIGHT_METERS,
-                          CAMERA_PITCH_RADIANS,
-                          Units.degreesToRadians(RRresult.getBestTarget().getPitch()));
-
-          // Use this range as the measurement we give to the PID controller.
-          // -1.0 required to ensure positive PID controller effort _increases_ range
-          forwardSpeed = -forwardController.calculate(range, GOAL_RANGE_METERS);
-
-          // Also calculate angular power
-          // -1.0 required to ensure positive PID controller effort _increases_ yaw
-          rotationSpeed = -turnController.calculate(RRresult.getBestTarget().getYaw(), 0);
-      } else {
-          // If we have no targets, stay still.
-          System.out.println("Could Not Find RR Target");
-          forwardSpeed = 0;
-          rotationSpeed = 0;
-          camera.setLED(VisionLEDMode.kOff);
-          //AprilTag Vision Mode
-          camera.setLED(VisionLEDMode.kBlink);
-        try {
-          wait(100);
-        } catch (InterruptedException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
-        camera.setLED(VisionLEDMode.kOff);
-        try {
-          wait(500);
-        } catch (InterruptedException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
-      // Vision-alignment mode
-      // Query the latest result from PhotonVision
-      var ATresult = camera.getLatestResult();
-      System.out.println("Finding AT Target");
-      if (RRresult.hasTargets()) {
-        System.out.println("AT Target Found");
-          // First calculate range
-          double range =
-                  PhotonUtils.calculateDistanceToTargetMeters(
-                          CAMERA_HEIGHT_METERS,
-                          TARGET_HEIGHT_METERS,
-                          CAMERA_PITCH_RADIANS,
-                          Units.degreesToRadians(ATresult.getBestTarget().getPitch()));
-
-          // Use this range as the measurement we give to the PID controller.
-          // -1.0 required to ensure positive PID controller effort _increases_ range
-          forwardSpeed = -forwardController.calculate(range, GOAL_RANGE_METERS);
-
-          // Also calculate angular power
-          // -1.0 required to ensure positive PID controller effort _increases_ yaw
-          rotationSpeed = -turnController.calculate(ATresult.getBestTarget().getYaw(), 0);
-      }else{
-        System.out.println("Could Not Find AT Target");
-          forwardSpeed = 0;
-          rotationSpeed = 0;
-      }
-      
-          // Manual Driver Mode
-      forwardSpeed = -c_xbox1.getLeftY();
-      rotationSpeed = turn;
-
-  // Use our forward/turn speeds to control the drivetrain
-  m_Drive.arcadeDrive(forwardSpeed, rotationSpeed); 
+    // Use our forward/turn speeds to control the drivetrain
+    m_robotDrive.arcadeDrive(forwardSpeed, rotationSpeed);
 }
-}
-}
+    
+ /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
 }
