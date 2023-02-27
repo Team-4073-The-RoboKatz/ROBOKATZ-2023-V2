5fe40dcce1062127313f66e8a53c35eb1d96d58c
Timmy McInerney <Timmybud922@gmail.com>
2023-02-26 22:54:11 -0500

    removed Constants and tankdrive files

--- src/main/java/frc/robot/Constants.java
+++
@@ -1,9 +0,0 @@
-package frc.robot;
-
-public class Constants {
-    public static final int LeftPort = 1;
-    public static final int RightPort = 6;
-    //public static final int armPort = 5;
-    public static final int clawPort = 11;
-    public static final int pcmPort = 10;
-}
